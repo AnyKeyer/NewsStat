00b6b3155613791dcd0dc11ca45e122319633685
AnyKeyer <slejv710@gmail.com>
2025-09-23 16:31:43 +0200

    Fix base path for GitHub Pages: NewsAnalys -> NewsStat

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/NewsAnalys/',
+  base: '/NewsStat/',
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
